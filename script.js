b26ce1c5a44cd6eeb05026c9a4b951cfdcc737bf
Lucas Magalhães <imlucasmagalhaes@outlook.com>
2024-04-28 12:34:15 -0300

    function to delete task

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const input = document.getElementById('inputTask') //captura o valor inserido no
 
 //adiciona dinamicamente um item a minha lista
 function addTask(){
-    const ul = document.getElementById('taskList') //captura a minha lista
-    const li = document.createElement('li') //cria um item dinamicamente
-    li.innerText = input.value //inseri o texto que estava no input para o meu item da lista
-    li.classList.add('task') //adiciona uma classe ao meu item
-    ul.appendChild(li) //adicona o item a minha lista
+    if(input.value === ''){
+        alert('Please enter a task')
+    } else {
+        const ul = document.getElementById('taskList') //captura a minha lista
+        const li = document.createElement('li') //cria um item dinamicamente
+        li.innerText = input.value //inseri o texto que estava no input para o meu item da lista
+        li.classList.add('task') //adiciona uma classe ao meu item
+        li.classList.add('taskitens')
+        li.appendChild(createRemoveBtn())
+        ul.appendChild(li) //adicona o item a minha lista
+    }
+}
+
+//cria um botâo que remove a task
+function createRemoveBtn(){
+    const btn = document.createElement('button') //cria o botão dinamicamente
+    btn.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    return btn //retorna o botão criado
 }
 
 button.addEventListener('click', addTask) //captura um evento de click e chama a função para adicionar uma nova task
