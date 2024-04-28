9512c80d093bb1a502278938bed6ec9c06137d17
Lucas Magalhães <imlucasmagalhaes@outlook.com>
2024-04-28 17:37:14 -0300

    adding remove item function

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function addTask(){
         const li = document.createElement('li') //cria um item dinamicamente
         li.innerText = input.value //inseri o texto que estava no input para o meu item da lista
         li.classList.add('task') //adiciona uma classe ao meu item
-        li.classList.add('taskitens')
-        li.appendChild(createRemoveBtn())
+        li.classList.add('taskitens') //adiciona um segunda classe no meu item
+        li.appendChild(createRemoveBtn()) //adiciona um botâo para remover o item
         ul.appendChild(li) //adicona o item a minha lista
     }
 }
@@ -20,7 +20,16 @@ function addTask(){
 function createRemoveBtn(){
     const btn = document.createElement('button') //cria o botão dinamicamente
     btn.innerHTML = '<i class="fa-solid fa-trash"></i>'
+    removeTask(btn) //chama a minha função que remove itens da lista
     return btn //retorna o botão criado
+}
+
+//remove os itens da lista
+function removeTask(btn){
+    //recebe o botão que foi clicado como parâmetro e adiciona uma função anonima para ele
+    btn.addEventListener('click', function (){
+        btn.parentNode.remove() //acessa o elemento pai do botâo que é li e remove ele da lista
+    })
 }
 
 button.addEventListener('click', addTask) //captura um evento de click e chama a função para adicionar uma nova task
