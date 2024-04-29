4024aba8f77055fbd5fa5c36ed37cd1ff1486e51
Lucas Magalhães <imlucasmagalhaes@outlook.com>
2024-04-29 14:45:18 -0300

    task check

--- script.js
+++ script.js
@@ -14,12 +14,15 @@ function addTask(){
         li.classList.add('task') //adiciona uma classe ao meu item
         li.classList.add('taskitens') //adiciona um segunda classe no meu item
         div.classList.add('organization') //adicona uma classe a minha div organizadora
+        const span = document.createElement('span') //cria uma tag span dinamicamente
+        span.textContent = input.value //atribui o valor do texto para minha tag span
         div.appendChild(radio) //adicona o meu radio button a minha div organizadora
-        div.appendChild(document.createTextNode(' '))
-        div.appendChild(document.createTextNode(input.value)) //coloca o texto que estava no input na minha div organizadora
+        div.appendChild(document.createTextNode(' ')) //adiciona um espaço entre o radio button e o texto
+        div.appendChild(span) //coloca o texto que estava no input na minha div organizadora
         li.appendChild(div) //adiciona a minha div organizadora ao meu li
         li.appendChild(createRemoveBtn()) //adiciona um botâo para remover o item
         ul.appendChild(li) //adicona o item a minha lista
+        checkTask(radio, span) //chama a função para verificar se o botâo foi marcado
     }
 }
 
@@ -36,6 +39,18 @@ function removeTask(btn){
     //recebe o botão que foi clicado como parâmetro e adiciona uma função anonima para ele
     btn.addEventListener('click', function (){
         btn.parentNode.remove() //acessa o elemento pai do botâo que é li e remove ele da lista
+    })
+}
+
+//verifica se a tarefa já foi concluída
+function checkTask(radioBtn, textTask){
+    radioBtn.addEventListener('click', function (){
+        //se o botão estiver com o valor checked ele adiciona uma linha por cima do meu texto
+        if(radioBtn.checked){
+            textTask.innerHTML = '<s>' + textTask.innerHTML + '</s>'
+        } else { //se não ele remove a linha
+            textTask.innerHTML = textTask.innerHTML.replace(/<\/?s>/g, '')
+        }
     })
 }
 
